46817ff3e16c08aa074c078a9345aa3cfda51e68
christophe13012 <christophecaillet@icloud.com>
2021-02-08 10:27:25 +0100

    auth token ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/api/login_check", name="api_login")
+     * @return JsonResponse
+     */
+    public function api_login(): JsonResponse
+    {
+        $user = $this->getUser();
+        return new JsonResponse([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
